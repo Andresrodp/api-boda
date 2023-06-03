956040a9b5d01508a892f7041b21e2090f28dcc9
Andresrodp <arodriguezp.cs.1@gmail.com>
2023-06-02 19:39:31 -0500

    cambio de puerto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import server from "./src/app.js";
 import database from "./src/db.js";
 
 
-server.listen(3000, () => {
+server.listen(3900, () => {
     database();
-    console.log("Server running on port 3000");
-    });
+    console.log("Server running on port 3900");
+});
