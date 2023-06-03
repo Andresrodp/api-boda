ef9b1cd13154ff64aaea0736dfff4a040f68e4a5
Andresrodp <arodriguezp.cs.1@gmail.com>
2023-06-02 19:49:46 -0500

    cambio en el Port

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import server from "./src/app.js";
 import database from "./src/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
+const PORT = process.env.PORT || 3900;
 
-server.listen(3900, () => {
+server.listen(PORT, "0.0.0.0", () => {
     database();
     console.log("Server running on port 3900");
 });
